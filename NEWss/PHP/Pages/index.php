127ac82b55865afd148bfa4c5657334b05a7c2e1
Neeli Satya Venkata Swaradeep <swaradeepneeli@gmail.com>
2020-02-21 22:05:51 +0530

    init

--- NEWss/PHP/Pages/index.php
+++ NEWss/PHP/Pages/index.php
@@ -3,56 +3,26 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>NEWss</title>
-<style>
-body, html{
-font-family:Segoe UI, Segoe UI Midlevel, Segoe WP, Arial, Sans-Serif;
-}
-.mytext{
-text-align: right;
-margin: 20px;
-}
-.contact{
-text-align: center;
-
-}
-.cards{
-float: left;
-}
-h2{
-margin: 0px;
-}
-.container{
-float: left;
-width:280px;
-margin-left: 15px;
-margin-right: 35px;
-margin-bottom: 40px;
-box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-transition: 0.3s;
-}
-.container:hover{
-box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
-.bottom{
- font-size: 25px;
- padding-left: 15px;
- padding-bottom: 10px; 
-}
-img{
-width: 100%;
-}
-.title{
-color:#555555;
-margin: 10px;
-}
-</style>
+<link href="index.css" type="text/css" rel="stylesheet">
 <script src="readNews.php" type="text/javascript"></script>
 </head>
 <body>
-<div class="mytext">
+
+<div class="heading">
+<h1>NEWSs</h1>
+</div>
+
+<div class="menubar">
+<a href="#home">Home</a>
+<a href="#categories">Categories</a>
+<a href="#about" >About Us</a>
+<a href="#usage">How to use</a>
+<a href="#contact">Contact</a>
+</div>
+
+<div class="menubar">
 <a href="AdminLogin.html" class="admin">Admin? Login here!</a><br>
 <a href="PostNews.html">Post a new NEWss</a>
-
 </div>
 <?php
 $conn = new mysqli("localhost", "root", "12345", "myDB");
@@ -63,14 +33,14 @@ if ($result->num_rows > 0) {
     echo "<div class='cards'>"; 
     while($row = $result->fetch_assoc()) {
 		$id = $row["idnews"];
-		$image = "'".$row["imageurl"]."'";
+		$image = "'images/".$row["imageurl"]."'";
 		$title = "'" .$row["title"]."'";
 		$desc = $row["description"];
 		$writer = $row["writer"];
 		//echo "<br>$id $image $title $desc $writer<br>";
     	//echo "<img onClick=setUrl(" .$id .','."'". $image."',encodeURIComponent('".$title."'),encodeURIComponent('".$desc."'),encodeURIComponent('".$writer."')); src=" .$row["imageurl"]. " width=380 height=250 name=".$row["idnews"].">".$row["title"];
     	echo "<div class=container>
-                <img onClick=setUrl(" .$image .',' .$title .") src=" .$row["imageurl"] ." name=".$row["idnews"].">
+                <img onClick=setUrl(" .$image .',' .$title .") src=" .$image ." name=".$row["idnews"].">
                 <div class='bottom'><p class='title' ><b> ".$row["title"]."</b></p></div>
               </div>";
     }
@@ -81,13 +51,22 @@ echo "</div>";
 
 $conn->close();
 ?>
+
 <script type="text/javascript">
 function setUrl(image,title){
 	document.writeln(image," ",title);
 }
 </script>
 <br><br><br>
-<div class="contact"><p class="contact"><h2><b>Contact:</b></h2>Swaradeep Neeli<br>Aditya Degree College<br>Rajahmundry<br>+91 9 6180 61803<br>swaradeepneeli@gmail.com	</p></div>
+
+<div class="contact">
+		<p><h2><b>Contact:</b></h2>
+		<p id="contact">Swaradeep Neeli</p>
+		<p>Aditya Degree College</p>
+		<p>Rajahmundry</p>
+		<p>+91 9 6180 61803</p>
+		<p>swaradeepneeli@gmail.com</p><br>
+</div>
 
 
 </body>
