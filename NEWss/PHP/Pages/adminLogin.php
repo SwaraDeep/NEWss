6d2f20155d9687b225f937e82a07e9916c9171ac
Neeli Satya Venkata Swaradeep <swaradeepneeli@gmail.com>
2020-02-23 18:12:28 +0530

    init

--- NEWss/PHP/Pages/adminLogin.php
+++ NEWss/PHP/Pages/adminLogin.php
@@ -1,16 +1,38 @@
 <html>
 <head>
-<style >
-form, b{
-padding: 10px;
-}
-input{
-width: 150px;
-}
-</style>
-</head>
-</html>
 
+
+<title>NEWss - Admin Login</title>
+<link href="css/index.css" type="text/css" rel="stylesheet">
+<link href="css/login.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<div class="header">
+<div class="heading">
+<h1>NEWSs</h1>
+</div>
+
+<div class="menubar">
+<a href="index.php">Home</a>
+<a href="PostNews.html">Upload</a>
+<select class="categories">
+<option>Select Category</option>
+<option>Politics</option>
+<option>Science</option>
+<option>Technology</option>
+<option>Cinema</option>
+<option>Wheather</option>
+<option>Sports</option>
+</select>
+<a href="about.html">About Us</a>
+<a href="#login" class="admin">Login</a>
+<a href="howtouse.html">How to use</a>
+<a href="contact.html">Contact</a>
+</div>
+</div>
+
+<div class="body">
+<div class="login" id="login">
 <?php
 try {
 $name = $_POST["name"];
@@ -21,23 +43,25 @@ $pass = $_POST["pass"];
 $name = sha1($name);
 $pass = sha1($pass);
 if($name === "d033e22ae348aeb5660fc2140aec35850c4da997" && $pass === "d033e22ae348aeb5660fc2140aec35850c4da997"){
-    echo "<b>Create Database:</b><form action='createdb.php'> <input type='submit' value='Create DB'>
+    echo "<b>Create Database:</b>
+<form action='createdb.php'>
+<input class=btn type='submit' value='Create DB'>
 </form><br>
 <b>Create Table:</b>
 <form action='createtable.php'>
-	<input type='submit' value='Create Table'>
+	<input class=btn type='submit' value='Create Table'>
 </form><br>
-<b>Drop Database:</b>
+<b>Delete Rows:</b>
 <form action='truncateTable.php'>
-	<input type='submit' value='Delete Rows'>
+	<input class=btn type='submit' value='Delete Rows'>
 </form><br>
 <b>Drop Table:</b>
 <form action='droptable.php'>
-	<input type='submit' value='Drop Table'>
+	<input class=btn type='submit' value='Drop Table'>
 </form><br>
 <b>Drop Database:</b>
 <form action='dropdatabase.php'>
-	<input type='submit' value='Drop Database'>
+	<input class=btn type='submit' value='Drop Database'>
 </form>";
 }else{
     echo "Invalid Credentials!";
@@ -45,3 +69,7 @@ if($name === "d033e22ae348aeb5660fc2140aec35850c4da997" && $pass === "d033e22ae3
 
 
 ?>
+</div>
+</div>
+</body>
+</html>
--- NEWss/PHP/Pages/index.php
+++ NEWss/PHP/Pages/index.php
@@ -3,27 +3,37 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>NEWss</title>
-<link href="index.css" type="text/css" rel="stylesheet">
+<link href="css/index.css" type="text/css" rel="stylesheet">
+<link rel="stylesheets" href="css/login.css" type="text/css">
 <script src="readNews.php" type="text/javascript"></script>
 </head>
 <body>
 
+<div class="header">
 <div class="heading">
 <h1>NEWSs</h1>
 </div>
 
 <div class="menubar">
 <a href="#home">Home</a>
-<a href="#categories">Categories</a>
-<a href="#about" >About Us</a>
-<a href="#usage">How to use</a>
-<a href="#contact">Contact</a>
+<a href="PostNews.html">Upload</a>
+<select class="categories">
+<option>Select Category</option>
+<option>Politics</option>
+<option>Science</option>
+<option>Technology</option>
+<option>Cinema</option>
+<option>Wheather</option>
+<option>Sports</option>
+</select>
+<a href="about.html">About Us</a>
+<a href="AdminLogin.html" class="admin">Login</a>
+<a href="howtouse.html">How to use</a>
+<a href="contact.html">Contact</a>
+</div>
 </div>
 
-<div class="menubar">
-<a href="AdminLogin.html" class="admin">Admin? Login here!</a><br>
-<a href="PostNews.html">Post a new NEWss</a>
-</div>
+<div class="data">
 <?php
 $conn = new mysqli("localhost", "root", "12345", "myDB");
 $que = "SELECT * FROM news;";
@@ -32,25 +42,24 @@ $result = $conn->query($que);
 if ($result->num_rows > 0) {
     echo "<div class='cards'>"; 
     while($row = $result->fetch_assoc()) {
-		$id = $row["idnews"];
 		$image = "'images/".$row["imageurl"]."'";
 		$title = "'" .$row["title"]."'";
-		$desc = $row["description"];
-		$writer = $row["writer"];
 		//echo "<br>$id $image $title $desc $writer<br>";
     	//echo "<img onClick=setUrl(" .$id .','."'". $image."',encodeURIComponent('".$title."'),encodeURIComponent('".$desc."'),encodeURIComponent('".$writer."')); src=" .$row["imageurl"]. " width=380 height=250 name=".$row["idnews"].">".$row["title"];
     	echo "<div class=container>
-                <img onClick=setUrl(" .$image .',' .$title .") src=" .$image ." name=".$row["idnews"].">
+                <img onClick=setUrl(" .$image .',' .$title .") height=200px src=" .$image ." name=".$row["idnews"].">
                 <div class='bottom'><p class='title' ><b> ".$row["title"]."</b></p></div>
               </div>";
     }
-echo "</div>";
+    echo "</div>";
 } else {
-    echo "0 results";
+    echo "<div class=body>
+<div class=login id=login><p>No articles yet</p></div></div>";
 }
 
 $conn->close();
 ?>
+</div>
 
 <script type="text/javascript">
 function setUrl(image,title){
@@ -58,16 +67,6 @@ function setUrl(image,title){
 }
 </script>
 <br><br><br>
-
-<div class="contact">
-		<p><h2><b>Contact:</b></h2>
-		<p id="contact">Swaradeep Neeli</p>
-		<p>Aditya Degree College</p>
-		<p>Rajahmundry</p>
-		<p>+91 9 6180 61803</p>
-		<p>swaradeepneeli@gmail.com</p><br>
-</div>
-
 
 </body>
 </html>
